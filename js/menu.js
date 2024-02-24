40eb1b19b9a79a7748136111ef67b2431cc2b038
infogrupomint <152441171+infogrupomint@users.noreply.github.com>
2024-02-24 16:54:06 -0300

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+const pizzaBtn = document.getElementById("btn-pizza");
+const empanadasBtn = document.getElementById("btn-empanadas");
+const sandwichesBtn = document.getElementById("btn-sandwiches");
+const bebidasBtn = document.getElementById("btn-bebidas");
+
+const displayCart = () =>{
+ alert("Estamos trabajando para brindarte un mejor servicio");
+}
+
+pizzaBtn.addEventListener("click", displayCart);
+empanadasBtn.addEventListener("click", displayCart);
+sandwichesBtn.addEventListener("click", displayCart);
+bebidasBtn.addEventListener("click", displayCart);
+
+
+
---
+++ js/products.js
@@ -0,0 +1,24 @@
+//Array de objetos para los productos
+const productos = [
+    {
+        id: 1,
+        productName:"Sandia",
+        price: 10,
+        quanty: 1,
+        img: "./media/sandia.jpg"
+    },
+    {
+        id: 2,
+        productName:"Bananita",
+        price: 20,
+        quanty: 1,
+        img: "./media/bananita.jpg"
+    },
+    {
+        id: 3,
+        productName:"Cervecita",
+        price: 30,
+        quanty: 1,
+        img: "./media/cervecita.jpg"
+    },
+]
